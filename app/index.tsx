b075da034c8b572d6bfd6a37b67abbbe96640a08
HadassaAvimorNew <ateretrimonim430@gmail.com>
2024-10-15 13:15:16 +0300

    update index.tsx location

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Platform } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+    <>
+        <p style={styles.title}>for the meantime 🤨</p>
+        </>
+  );
+}
+
+const styles = StyleSheet.create({
+  title: {
+   color:"red",
+  },
+
+});
